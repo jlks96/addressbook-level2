730b8d49e70505d4308ba0329227bbc773b36e1f
DESKTOP-1NFIFHB\Joshua <joshlee_1996@hotmail.com>
2018-02-12 18:30:06 +0800

    -abstracted out Formatter class from TextUi class

---
+++ src/seedu/addressbook/ui/Formatter.java
@@ -0,0 +1,66 @@
+package seedu.addressbook.ui;
+
+import seedu.addressbook.commands.CommandResult;
+import seedu.addressbook.data.person.ReadOnlyPerson;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static seedu.addressbook.common.Messages.*;
+import static seedu.addressbook.common.Messages.MESSAGE_INIT_FAILED;
+
+public class Formatter {
+
+    /** A decorative prefix added to the beginning of lines printed by AddressBook */
+    private static final String LINE_PREFIX = "|| ";
+
+    /** A platform independent line separator. */
+    private static final String LS = System.lineSeparator();
+
+
+    /** Format of indexed list item */
+    private static final String MESSAGE_INDEXED_LIST_ITEM = "\t%1$d. %2$s";
+
+
+    /** Offset required to convert between 1-indexing and 0-indexing.  */
+    public static final int DISPLAYED_INDEX_OFFSET = 1;
+
+    /**
+     * Formats user input prompt to be printed
+     * @return user input prompt to be printed in formatted form
+     */
+    public static String formatUserInputPrompt() {
+        return LINE_PREFIX + "Enter command: ";
+    }
+
+    /**
+     * Formats a string as a viewable indexed list item.
+     *
+     * @param visibleIndex visible index for this listing
+     */
+    public static String formatIndexedListItem(int visibleIndex, String listItem) {
+        return String.format(MESSAGE_INDEXED_LIST_ITEM, visibleIndex, listItem);
+    }
+
+    /** Formats a list of strings as a viewable indexed list. */
+    public static String formatIndexedListForViewing(List<String> listItems) {
+        final StringBuilder formatted = new StringBuilder();
+        int displayIndex = 0 + DISPLAYED_INDEX_OFFSET;
+        for (String listItem : listItems) {
+            formatted.append(formatIndexedListItem(displayIndex, listItem)).append("\n");
+            displayIndex++;
+        }
+        return formatted.toString();
+    }
+
+    /**
+     * Formats a message to be printed
+     * @param message message to be printed unformatted
+     * @return message to be printed in formatted form
+     */
+    public static String formatShowToUser(String message) {
+        return LINE_PREFIX + message.replace("\n", LS + LINE_PREFIX);
+    }
+
+}
